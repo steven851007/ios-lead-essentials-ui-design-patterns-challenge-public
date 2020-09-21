dae6a21a82cb673907d38fc6fa265236fee48953
Caio Zullo <caiozullo@gmail.com>
2020-09-21 15:53:24 +0100

    Replace #file with the new #filePath

--- MVC Module/Tests/Feed UI/Helpers/FeedUIIntegrationTests+Assertions.swift
+++ MVC Module/Tests/Feed UI/Helpers/FeedUIIntegrationTests+Assertions.swift
@@ -8,7 +8,7 @@ import FeedFeature
 
 extension FeedUIIntegrationTests {
 
-	func assertThat(_ sut: FeedViewController, isRendering feed: [FeedImage], file: StaticString = #file, line: UInt = #line) {
+	func assertThat(_ sut: FeedViewController, isRendering feed: [FeedImage], file: StaticString = #filePath, line: UInt = #line) {
 		guard sut.numberOfRenderedFeedImageViews() == feed.count else {
 			return XCTFail("Expected \(feed.count) images, got \(sut.numberOfRenderedFeedImageViews()) instead.", file: file, line: line)
 		}
@@ -18,7 +18,7 @@ extension FeedUIIntegrationTests {
 		}
 	}
 	
-	func assertThat(_ sut: FeedViewController, hasViewConfiguredFor image: FeedImage, at index: Int, file: StaticString = #file, line: UInt = #line) {
+	func assertThat(_ sut: FeedViewController, hasViewConfiguredFor image: FeedImage, at index: Int, file: StaticString = #filePath, line: UInt = #line) {
 		let view = sut.feedImageView(at: index)
 		
 		guard let cell = view as? FeedImageCell else {
