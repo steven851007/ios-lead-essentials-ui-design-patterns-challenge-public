adadfcfca3c7e9686a6084fc3c8d1a24a0dd044e
Istvan <istvan.balogh@outlook.com>
2021-10-15 13:32:06 +0200

    Implement error message mocking for snapshot UI test and add missing localizations

--- MVVM Module/Tests/Feed UI/FeedUISnapshotTests.swift
+++ MVVM Module/Tests/Feed UI/FeedUISnapshotTests.swift
@@ -23,14 +23,14 @@ class FeedUISnapshotTests: XCTestCase {
 		assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "EMPTY_FEED_dark")
 	}
 
-//	func test_feedWithError() {
-//		let sut = makeSUT()
-//
-//		sut.display(errorMessage: "An error message")
-//
-//		assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_ERROR_light")
-//		assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_ERROR_dark")
-//	}
+	func test_feedWithError() {
+		let sut = makeSUT()
+
+		sut.display(errorMessage: "An error message")
+
+		assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_ERROR_light")
+		assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_ERROR_dark")
+	}
 
 	// MARK: - Helpers
 
@@ -65,7 +65,7 @@ private class FeedLoaderStub: FeedLoader {
 
 private extension FeedViewController {
 	func display(errorMessage: String) {
-		fatalError("Must be implemented - follow the MVC solution as a guide")
+		errorView.show(message: errorMessage)
 	}
 
 	func display(_ feed: [FeedImageCellController]) {
