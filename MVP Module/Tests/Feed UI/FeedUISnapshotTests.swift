ac808374506da1847f2c6a893dccea3f1a485d19
Istvan <istvan.balogh@outlook.com>
2021-10-15 14:45:12 +0200

    Enable and fix MPV snapshot tests

--- MVP Module/Tests/Feed UI/FeedUISnapshotTests.swift
+++ MVP Module/Tests/Feed UI/FeedUISnapshotTests.swift
@@ -14,23 +14,24 @@ class FeedUISnapshotTests: XCTestCase {
 	//
 	//  ***********************
 
-//	func test_emptyFeed() {
-//		let sut = makeSUT()
+	func test_emptyFeed() {
+		let sut = makeSUT()
+
+		sut.display(emptyFeed())
+
+		assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "EMPTY_FEED_light")
+		assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "EMPTY_FEED_dark")
+	}
+
 //
-//		sut.display(emptyFeed())
-//
-//		assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "EMPTY_FEED_light")
-//		assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "EMPTY_FEED_dark")
-//	}
-//
-//	func test_feedWithError() {
-//		let sut = makeSUT()
-//
-//		sut.display(errorMessage: "An error message")
-//
-//		assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_ERROR_light")
-//		assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_ERROR_dark")
-//	}
+	func test_feedWithError() {
+		let sut = makeSUT()
+
+		sut.display(errorMessage: "An error message")
+
+		assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_ERROR_light")
+		assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_ERROR_dark")
+	}
 
 	// MARK: - Helpers
 
@@ -51,6 +52,6 @@ class FeedUISnapshotTests: XCTestCase {
 
 private extension FeedViewController {
 	func display(errorMessage: String) {
-		fatalError("Must be implemented - follow the MVC solution as a guide")
+		errorView.show(message: errorMessage)
 	}
 }
